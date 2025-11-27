5131e1c9812a1e969ad149cf384fcc5b1fb270da
Yuri Kuniyoshi <e235750@ie.u-ryukyu.ac.jp>
2025-11-27 17:16:12 +0900

    不要なルートを削除

--- spec/helpers/profiles_helper_spec.rb
+++ spec/helpers/profiles_helper_spec.rb
@@ -22,18 +22,8 @@ RSpec.describe ProfilesHelper, type: :helper do
   end
 
   describe "#likes_tab_path" do
-    context "params[:id]が存在する場合" do
-      before { params[:id] = user.id.to_s }
-
-      it "user_profile_likes_pathを返す" do
-        expect(helper.likes_tab_path).to eq(user_profile_likes_path(user.id))
-      end
-    end
-
-    context "params[:id]が存在しない場合" do
-      it "profile_likes_pathを返す" do
-        expect(helper.likes_tab_path).to eq(profile_likes_path)
-      end
+    it "profile_likes_pathを返す" do
+      expect(helper.likes_tab_path).to eq(profile_likes_path)
     end
   end
 
@@ -52,15 +42,13 @@ RSpec.describe ProfilesHelper, type: :helper do
   end
 
   describe "#likes_tab_active?" do
-    before { params[:id] = user.id.to_s }
-
     it "いいねページで表示される場合、trueを返す" do
-      allow(helper).to receive(:current_page?).with(user_profile_likes_path(user.id)).and_return(true)
+      allow(helper).to receive(:current_page?).with(profile_likes_path).and_return(true)
       expect(helper.likes_tab_active?).to be true
     end
 
     it "いいねページでない場合、falseを返す" do
-      allow(helper).to receive(:current_page?).with(user_profile_likes_path(user.id)).and_return(false)
+      allow(helper).to receive(:current_page?).with(profile_likes_path).and_return(false)
       expect(helper.likes_tab_active?).to be false
     end
   end
--- spec/requests/profiles_spec.rb
+++ spec/requests/profiles_spec.rb
@@ -116,24 +116,4 @@ RSpec.describe "Profiles", type: :request do
       end
     end
   end
-
-  describe "GET /users/:id/profile/likes" do
-    before { sign_in user, scope: :user }
-
-    it "他ユーザーのいいね一覧はアクセス不可" do
-      get user_profile_likes_path(other_user)
-
-      expect(response).to redirect_to(user_profile_path(other_user))
-      follow_redirect!
-      expect(response.body).to include("他のユーザーのいいね一覧は閲覧できません")
-    end
-
-    it "自分のいいね一覧は閲覧可能" do
-      create(:like, user: user, post: liked_post)
-      get user_profile_likes_path(user)
-
-      expect(response).to have_http_status(:success)
-      expect(response.body).to include(liked_post.content)
-    end
-  end
 end
