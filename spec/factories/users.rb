113e5d6a88a1d9df453f7c3842fdaee2885b6de8
Tomonori <150645282+Tomo-akm@users.noreply.github.com>
2025-12-11 03:03:50 +0900

    feat: Userテーブルから希望・所属研究室を削除する

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,13 +5,11 @@ FactoryBot.define do
     password_confirmation { "password123" }
     name { Faker::Name.name }
     favorite_language { %w[Ruby Python JavaScript Go].sample }
-    research_lab { Faker::Company.name }
     internship_count { rand(0..10) }
     personal_message { Faker::Lorem.sentence }
 
     trait :without_profile do
       favorite_language { nil }
-      research_lab { nil }
       internship_count { nil }
       personal_message { nil }
     end
