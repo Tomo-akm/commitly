42fdc2979fc001fea9de261785b5bfdf912ce6e3
Tomonori <150645282+Tomo-akm@users.noreply.github.com>
2025-12-11 03:00:34 +0900

    feat: Profile欄から希望・所属研究室を削除する

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -44,7 +44,7 @@ class ProfilesController < ApplicationController
   end
 
   def profile_params
-    params.expect(user: [ :name, :favorite_language, :research_lab, :internship_count, :personal_message ])
+    params.expect(user: [ :name, :favorite_language, :internship_count, :personal_message ])
   end
 
   def prepare_heatmap_data
