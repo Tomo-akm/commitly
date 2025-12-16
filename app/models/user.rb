12246377b99304aca32681b94dc1f967623aa8e4
津波柊斗 <tsuha_shuto@tsunamihiiragitonoMacBook-Air-2.local>
2025-12-16 15:36:06 +0900

    refactor: ユーザアイコンのリファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -60,7 +60,7 @@ class User < ApplicationRecord
       return "https://api.dicebear.com/8.x/bottts/svg?seed=#{avatar_seed}&size=#{size}"
     end
     Rails.application.routes.url_helpers.rails_representation_url(
-      avatar.variant(resize_to_limit: [size, size]).processed,
+      avatar.variant(resize_to_limit: [ size, size ]).processed,
       only_path: true
     )
   end
